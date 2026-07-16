be5c14eeaa9c6f9a6fbdab4dd7b023b846175015
Wilbert <wilberttan1121@gmail.com>
2026-07-17 02:20:05 +0800

    feat: set up l10n (en/ms/zh) with context shorthand

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kongsi/app/theme/app_theme.dart';
 import 'package:kongsi/core/di/core_providers.dart';
+import 'package:kongsi/l10n/gen/app_localizations.dart';
+import 'package:kongsi/l10n/l10n_extension.dart';
 
 void main() {
   runApp(
@@ -16,6 +18,9 @@ class MainApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final config = ref.watch(appConfigProvider);
     return MaterialApp(
+      onGenerateTitle: (context) => context.l10n.appTitle,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       // themeMode defaults to ThemeMode.system; user-controllable later.
