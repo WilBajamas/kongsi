38b525e7b1b31f52967eb0d9db4961b67b49e089
Wilbert <wilberttan1121@gmail.com>
2026-07-19 23:52:23 +0800

    test: skip platform goldens on CI

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:alchemist/alchemist.dart';
 import 'package:kongsi/app/theme/app_theme.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  final isCI = Platform.environment.containsKey('CI');
   return AlchemistConfig.runWithConfig(
-    config: AlchemistConfig(theme: AppTheme.light),
+    config: AlchemistConfig(
+      theme: AppTheme.light,
+      platformGoldensConfig: PlatformGoldensConfig(enabled: !isCI),
+    ),
     run: testMain,
   );
 }
